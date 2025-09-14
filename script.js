ce96aca88dd8d7010494e77ace70c27480b15b48
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-09-14 17:25:07 +0500

    ok

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const searchbtn = document.getElementById("search-btn");
     const userInput = document.getElementById("user-input");
     const statsContainer = document.querySelector(".stats-container");
-    const easyprogressCircle = document.querySelector(".easy-progress");   // class
+    const easyprogressCircle = document.querySelector(".easy-progress"); 
     const mediumprogressCircle = document.querySelector(".medium-progress");
     const hardprogressCircle = document.querySelector(".hard-progress");
     const easyLabel = document.getElementById("easy-label");
@@ -16,47 +16,47 @@ document.addEventListener("DOMContentLoaded", function() {
 
         for (let i = 0; i < arr.length; i++) {
             if (userInput.value.toLowerCase() === arr[0].toLowerCase()) {
-                easyprogressCircle.innerHTML = "Easy<br><br>50%";
-                 mediumprogressCircle.innerHTML = "Medium <br> <br> 30%";
-                 hardprogressCircle.innerHTML = "Hard <br><br> 20%"
+                easyprogressCircle.innerHTML = "Easy<br><br>35%";
+                 mediumprogressCircle.innerHTML = "Medium <br> <br> 51%";
+                 hardprogressCircle.innerHTML = "Hard <br><br> 14%"
                 found = true;
-                break; // exit loop once found
+                break; 
             }
              if (userInput.value.toLowerCase() === arr[1].toLowerCase()) {
-                easyprogressCircle.innerHTML = "Easy<br><br>60%";
+                easyprogressCircle.innerHTML = "Easy<br><br>30%";
                  mediumprogressCircle.innerHTML = "Medium <br> <br> 20%";
-                 hardprogressCircle.innerHTML = "Hard <br><br> 20%"
+                 hardprogressCircle.innerHTML = "Hard <br><br> 50%"
                 found = true;
-                break; // exit loop once found
+                break; 
             }
               if (userInput.value.toLowerCase() === arr[2].toLowerCase()) {
-                easyprogressCircle.innerHTML = "Easy<br><br>10%";
-                 mediumprogressCircle.innerHTML = "Medium <br> <br> 70%";
+                easyprogressCircle.innerHTML = "Easy<br><br>19%";
+                 mediumprogressCircle.innerHTML = "Medium <br> <br> 71%";
                  hardprogressCircle.innerHTML = "Hard <br><br> 20%"
                 found = true;
-                break; // exit loop once found
+                break; 
             }
               if (userInput.value.toLowerCase() === arr[3].toLowerCase()) {
                 easyprogressCircle.innerHTML = "Easy<br><br>30%";
-                 mediumprogressCircle.innerHTML = "Medium <br> <br> 30%";
-                 hardprogressCircle.innerHTML = "Hard <br><br> 40%"
+                 mediumprogressCircle.innerHTML = "Medium <br> <br> 25%";
+                 hardprogressCircle.innerHTML = "Hard <br><br> 60%"
                 found = true;
-                break; // exit loop once found
+                break; 
             }
               if (userInput.value.toLowerCase() === arr[4].toLowerCase()) {
                 easyprogressCircle.innerHTML = "Easy<br><br>20%";
                  mediumprogressCircle.innerHTML = "Medium <br> <br> 40%";
                  hardprogressCircle.innerHTML = "Hard <br><br> 40%"
                 found = true;
-                break; // exit loop once found
+                break; 
             }
         }
         
 
         if (!found) {
-            easyprogressCircle.innerHTML = "Not found ❌";
-            mediumprogressCircle.innerHTML = "Not found ❌";
-            hardprogressCircle.innerHTML = "Not found ❌";
+            easyprogressCircle.innerHTML = "Not Found ❌";
+            mediumprogressCircle.innerHTML = "Not Found ❌";
+            hardprogressCircle.innerHTML = "Not Found ❌";
         }
         
         
