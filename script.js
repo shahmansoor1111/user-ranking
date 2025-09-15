2fc217c1c5b0e8f7441e26d5526ae2cf145f748d
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-09-15 09:08:42 +0500

    ok

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const cardstatsContainer = document.querySelector(".stats-card");
 
     searchbtn.addEventListener("click", function() {
-        let arr = ["shahmansoor","faraz", "abdulwahab","tatheer", "umama"];
+        let arr = ["shahmansoor","faraz", "abdulwahab","tatheer", "moiz"];
         let found = false;
 
         for (let i = 0; i < arr.length; i++) {
@@ -19,34 +19,52 @@ document.addEventListener("DOMContentLoaded", function() {
                 easyprogressCircle.innerHTML = "Easy<br><br>35%";
                  mediumprogressCircle.innerHTML = "Medium <br> <br> 51%";
                  hardprogressCircle.innerHTML = "Hard <br><br> 14%"
+                 easyprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,35%), #283a2e 0%)";
+                  mediumprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,51%), #283a2e 0%)";
+                   hardprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,14%), #283a2e 0%)";
                 found = true;
                 break; 
             }
              if (userInput.value.toLowerCase() === arr[1].toLowerCase()) {
                 easyprogressCircle.innerHTML = "Easy<br><br>30%";
+            
+
                  mediumprogressCircle.innerHTML = "Medium <br> <br> 20%";
                  hardprogressCircle.innerHTML = "Hard <br><br> 50%"
+                 easyprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,30%), #283a2e 0%)";
+                  mediumprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,20%), #283a2e 0%)";
+                   hardprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,50%), #283a2e 0%)";
                 found = true;
                 break; 
             }
               if (userInput.value.toLowerCase() === arr[2].toLowerCase()) {
+                
                 easyprogressCircle.innerHTML = "Easy<br><br>19%";
                  mediumprogressCircle.innerHTML = "Medium <br> <br> 71%";
                  hardprogressCircle.innerHTML = "Hard <br><br> 20%"
+                 easyprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,19%), #283a2e 0%)";
+                  mediumprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,71%), #283a2e 0%)";
+                   hardprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,20%), #283a2e 0%)";
                 found = true;
                 break; 
             }
               if (userInput.value.toLowerCase() === arr[3].toLowerCase()) {
                 easyprogressCircle.innerHTML = "Easy<br><br>30%";
                  mediumprogressCircle.innerHTML = "Medium <br> <br> 25%";
-                 hardprogressCircle.innerHTML = "Hard <br><br> 60%"
+                 hardprogressCircle.innerHTML = "Hard <br><br> 45%"
                 found = true;
+                easyprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,30%), #283a2e 0%)";
+                  mediumprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,25%), #283a2e 0%)";
+                   hardprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,45%), #283a2e 0%)";
                 break; 
             }
               if (userInput.value.toLowerCase() === arr[4].toLowerCase()) {
                 easyprogressCircle.innerHTML = "Easy<br><br>20%";
-                 mediumprogressCircle.innerHTML = "Medium <br> <br> 40%";
-                 hardprogressCircle.innerHTML = "Hard <br><br> 40%"
+                 mediumprogressCircle.innerHTML = "Medium <br> <br> 30%";
+                 hardprogressCircle.innerHTML = "Hard <br><br> 50%"
+                 easyprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,20%), #283a2e 0%)";
+                  mediumprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,40%), #283a2e 0%)";
+                   hardprogressCircle.style.background = "conic-gradient(#299f5d var(--progress-degree,40%), #283a2e 0%)";
                 found = true;
                 break; 
             }
